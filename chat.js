58c7077dbb6593d25a9de226780b6d0e81aaf2d1
Jason Koo <37762099+jason-koo@users.noreply.github.com>
2019-01-13 15:27:39 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,3 +1,4 @@
+//FRONTEND
 //make connection
 var socket = io.connect('http://localhost:4000');
 
@@ -5,7 +6,8 @@ var socket = io.connect('http://localhost:4000');
 var message = document.getElementById('message'),
       handle = document.getElementById('handle'),
       btn = document.getElementById('send'),
-      output = document.getElementById('output');
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
 
 
 //emit events
@@ -18,7 +20,16 @@ btn.addEventListener('click', function(){
   message.value = "";
 });
 
+message.addEventListener('keypress', function(){
+  socket.emit('typing', handle.value);
+});
+
 //listen for evnets
 socket.on('chat', function(data){
+  feedback.innerHTML= '';
   output.innerHTML +='<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+  feedback.innerHTML = '<p><em>' +data +' is typing a message...</em></p>';
 });
